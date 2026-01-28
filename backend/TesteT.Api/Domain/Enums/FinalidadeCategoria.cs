ed3da3b6edbb134265e059d245d97b5511a521d9
MCS-Lucas <lucascasilveira@gmail.com>
2026-01-28 20:38:50 -0300

    Fiz os enums pra começar, bem simples e de facil entendimento

---
+++ backend/TesteT.Api/Domain/Enums/FinalidadeCategoria.cs
@@ -0,0 +1,10 @@
+namespace TesteT.Api.Domain.Enums
+{
+    //Optei por criar enums separados para categoria e transação só para não misturar os conceitos.
+    public enum FinalidadeCategoria
+    {
+        Despesa = 1,
+        Receita = 2,
+        Ambas = 3
+    }
+}
---
+++ backend/TesteT.Api/Domain/Enums/TipoTransacao.cs
@@ -0,0 +1,9 @@
+namespace TesteT.Api.Domain.Enums
+{
+    //Apenas repetindo que optei por criar enums separados para categoria e transação só para não misturar os conceitos.
+    public enum TipoTransacao
+    {
+        Despesa = 1,
+        Receita = 2
+    }
+}
