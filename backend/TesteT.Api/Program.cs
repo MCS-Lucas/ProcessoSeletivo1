c11e7a49f4bdb38bce484f3177c82a5c077b7df1
MCS-Lucas <lucascasilveira@gmail.com>
2026-01-29 09:01:21 -0300

    Fui testar a controller e percebi que havia esquecido de declarar os metodos de implementação de interface na program.cs

--- backend/TesteT.Api/Program.cs
+++ backend/TesteT.Api/Program.cs
@@ -1,4 +1,6 @@
 using TesteT.Api.Infra.DependencyInjection;
+using TesteT.Api.Application.Interfaces;
+using TesteT.Api.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +20,11 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddInfra(builder.Configuration);
 
+// Aqui estão os métodos que irão dizer ao DI qual classe implementar quando uma interface for requisitada
+builder.Services.AddScoped<IPessoaService, PessoaService>();
+//builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+//builder.Services.AddScoped<ITransacaoService, TransacaoService>();
+//builder.Services.AddScoped<IRelatorioService, RelatorioService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -27,9 +34,10 @@ if (app.Environment.IsDevelopment())
     app.UseCors("DevCors");
 }
 
-// Deixei comentado porque a API está rodando em HTTP neste teste,
-// pra não depender de certificado HTTPS local.
-// app.UseHttpsRedirection();       
+/* Deixei comentado porque a API está rodando em HTTP neste teste,
+   pra não depender de certificado HTTPS local.
+
+   app.UseHttpsRedirection();    */   
 
 app.MapControllers();
 
